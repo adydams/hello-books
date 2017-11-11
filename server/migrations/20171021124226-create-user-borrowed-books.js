8657b6ce01ac117d7f7c1c57ebf8359fcd7f8225
adydams <adydams@gmail.com>
2017-11-11 16:29:36 +0100

    create models and migrations for borrowing books

---
+++ server/migrations/20171021124226-create-user-borrowed-books.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('UserBorrowedBooks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      bookId: {
+        type: Sequelize.INTEGER
+      },
+      borrowedDate: {
+        type: Sequelize.DATE
+      },
+      returnedDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('UserBorrowedBooks');
+  }
+};
